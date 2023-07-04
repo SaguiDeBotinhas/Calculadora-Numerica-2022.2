22649d8726afe6357ed962494ebcff65773ab6dc
Sagui de Botinhas <geu_costa@outlook.com>
2023-07-03 22:16:49 -0300

    Implementação do corpo do código

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# Primeiro Commit!! :D
+# OBJETIVOS - SEGUNDO COMMIT
+
+1° Implementação de um menu geral
+
+Esse menu sera construido de forma a partimentar o código e fracionar as funcionalidades/conteúdos que estaram disponiveis, no caso:
+ • Calculo de raizes
+
+    -Método da bisseção
+    -Método da secante
+    -Método do ponto fixo
+    -Método de Newton
+
+ • Interpolação
+
+ • Derivada
+
+ • Integral
+
+ • MMQ
+ 
+ • Solução de PVI's Simples
--- main.cpp
+++ main.cpp
@@ -1,8 +1,169 @@
 #include <iostream>
 
-int main() {
-    
-    std::cout << "Hi, its working!";
+int F_Menu(int Size, std::string *OP_Implementadas);
 
-    return 0;
+void F_Raizes();
+
+void F_Bisseccao();
+void F_Secante();
+void F_PontoFixo();
+void F_Newton();
+
+int main() {
+
+    int MConfig;
+    std::string OP_Funcionalidades[] = {"Calculo de raizes", "Interpolação", "Derivada", "Integral", "MMQ", "Solução de PVI's Simples"};
+
+    do {
+
+        MConfig = F_Menu(sizeof(OP_Funcionalidades) / sizeof (OP_Funcionalidades[0]), OP_Funcionalidades);
+
+        switch (MConfig) {
+
+            case 0:
+
+                std::cout << "Finalizando o programa... " << '\n';
+                system("pouse");
+                system("cls");
+
+                break;
+
+            case 1:
+
+                F_Raizes();
+                system("pouse");
+                system("cls");
+
+                break;
+
+            case 2:
+
+                
+                break;
+
+            case 3:
+
+                
+
+                break;
+
+            case 4:
+
+                
+
+                break;
+
+            default:
+
+                std::cout << "Insira um numero valido! " << '\n';
+                system("pouse");
+                system("cls");
+
+                break;
+        }
+        
+    } while (MConfig != 0);
+    
+
+    
+    
+        return 0;
+}
+
+int F_Menu(int Size, std::string *OP_Implementadas) {
+
+    int F_MConfig;
+
+    do {
+
+        std::cout << "Funcionalidades implementadas: " << '\n';
+
+        for (int cont0 = 0; cont0 < Size; cont0 += 1) {
+            std::cout << cont0 + 1 << " - " << OP_Implementadas[cont0] << '\n';
+        }
+        std::cout << "0 - Sair" << '\n';
+
+        std::cout << "Digite a opcao desejada: " << '\n';
+        std::cin >> F_MConfig;
+
+        if (F_MConfig <= 0 || F_MConfig >= Size)
+            std::cout << "Digite uma opção valida... " << '\n';
+
+    } while (!(F_MConfig >= 0 && F_MConfig <= Size));
+
+    return F_MConfig;
+}
+
+void F_Raizes() {
+    
+    int MConfig;
+    std::string OP_Metodos[] = {"Bissecao", "Secante", "Ponto Fixo", "Newton"};
+
+    MConfig = F_Menu(sizeof(OP_Metodos) / sizeof (OP_Metodos[0]), OP_Metodos);
+
+    switch (MConfig) {
+
+            case 0:
+
+                std::cout << "Finalizando o programa... " << '\n';
+                system("pouse");
+                system("cls");
+
+                break;
+
+            case 1:
+
+                F_Bisseccao();
+                system("pouse");
+                system("cls");
+
+                break;
+
+            case 2:
+
+                F_Secante();
+                system("pouse");
+                system("cls");
+
+                break;
+
+            case 3:
+
+                F_PontoFixo();
+                system("pouse");
+                system("cls");
+
+                break;
+
+            case 4:
+
+                F_Newton();
+                system("pouse");
+                system("cls");
+
+                break;
+
+            default:
+
+                std::cout << "Insira um numero valido! " << '\n';
+                system("pouse");
+                system("cls");
+
+                break;
+        }
+}
+void F_Bisseccao() {
+
+}
+
+void F_Secante() {
+
+}
+
+void F_PontoFixo() {
+
+}
+
+void F_Newton() {
+
 }
